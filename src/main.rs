652b7718d19ef499cc8ca1db962c63dbf4a6b0aa
Jeremy Banks <_@jeremy.ca>
2019-07-20 13:59:56 -0400

    get a moving

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,62 @@
 #![allow(unused_imports)]
 
+use std::convert::TryFrom;
 use std::fmt::Debug;
 
 use chess::{Board, BoardStatus, ChessMove, Color, Game, GameResult, MoveGen};
-use rand::{seq::SliceRandom, thread_rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use shakmaty as shak_chess;
 
 fn main() {
-    let board = Board::default();
+    let white = MoveForward;
+    let black = RandomAgent;
 
-    for _ in 0..16 {
-        let white = RandomAgent;
-        let black = RandomAgent;
+    play_n_games(100, MoveForward, RandomAgent);
+    play_n_games(100, MoveBackward, RandomAgent);
+    play_n_games(100, MoveForward, MoveBackward);
+}
+/// Plays N games between two agents, alternating sides, and returns the
+/// fraction which were won by player one.
+pub fn play_n_games(n: u32, player_one: impl Agent, player_two: impl Agent) -> f64 {
+    let mut player_one_wins = 0.0;
+    for i in 0..n {
+        let player_one = player_one.clone();
+        let player_two = player_two.clone();
 
-        println!(
-            "{:?} vs {:?}: {:?}",
-            &white,
-            &black,
-            play_game(white, black)
-        );
+        use chess::GameResult::*;
+        player_one_wins += if i % 2 == 0 {
+            match play_game(player_one, player_two) {
+                WhiteCheckmates => 1.0,
+                WhiteResigns => 0.0,
+                BlackCheckmates => 0.0,
+                BlackResigns => 1.0,
+                Stalemate => 0.5,
+                DrawAccepted => 0.5,
+                DrawDeclared => 0.5,
+            }
+        } else {
+            match play_game(player_two, player_one) {
+                WhiteCheckmates => 0.0,
+                WhiteResigns => 1.0,
+                BlackCheckmates => 1.0,
+                BlackResigns => 0.0,
+                Stalemate => 0.5,
+                DrawAccepted => 0.5,
+                DrawDeclared => 0.5,
+            }
+        }
     }
+    let ratio = player_one_wins / f64::from(n);
+
+    println!(
+        "{:?} vs {:?}: {:?} won {:.1}%",
+        &player_one,
+        &player_two,
+        &player_one,
+        ratio * 100.0
+    );
+
+    ratio
 }
 
 pub fn play_game(mut white: impl Agent, mut black: impl Agent) -> GameResult {
@@ -45,7 +82,7 @@ pub fn play_game(mut white: impl Agent, mut black: impl Agent) -> GameResult {
         assert!(game.make_move(*chosen_move), "illegal move?");
     }
 
-    game.result().unwrap()
+    game.result().expect("game must a result")
 }
 
 pub trait Agent: Debug + Clone {
@@ -54,6 +91,30 @@ pub trait Agent: Debug + Clone {
         board: &Board,
         moves: Vec<&'moves ChessMove>,
     ) -> &'moves ChessMove;
+}
+
+pub trait RankMove: Debug + Clone {
+    fn rank_move(&mut self, board: &Board, chess_move: &ChessMove) -> i64;
+}
+
+impl<T> Agent for T
+where
+    T: RankMove,
+{
+    fn pick_move<'moves>(
+        &mut self,
+        board: &Board,
+        mut moves: Vec<&'moves ChessMove>,
+    ) -> &'moves ChessMove {
+        moves.shuffle(&mut thread_rng());
+        moves.sort_by_cached_key(|candidate| {
+            (
+                self.rank_move(&board, &candidate),
+                thread_rng().gen::<i64>(),
+            )
+        });
+        moves[moves.len() - 1]
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -67,5 +128,39 @@ impl Agent for RandomAgent {
     ) -> &'moves ChessMove {
         moves.shuffle(&mut thread_rng());
         moves[0]
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct MoveForward;
+
+impl RankMove for MoveForward {
+    fn rank_move(&mut self, board: &Board, chess_move: &ChessMove) -> i64 {
+        let my_color = board.side_to_move();
+        let rank_shift = i64::try_from(chess_move.get_dest().get_rank().to_index()).unwrap()
+            - i64::try_from(chess_move.get_source().get_rank().to_index()).unwrap();
+        let forward_shift = if my_color == Color::White {
+            rank_shift
+        } else {
+            -rank_shift
+        };
+        forward_shift
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct MoveBackward;
+
+impl RankMove for MoveBackward {
+    fn rank_move(&mut self, board: &Board, chess_move: &ChessMove) -> i64 {
+        let my_color = board.side_to_move();
+        let rank_shift = i64::try_from(chess_move.get_dest().get_rank().to_index()).unwrap()
+            - i64::try_from(chess_move.get_source().get_rank().to_index()).unwrap();
+        let backward_shift = if my_color == Color::White {
+            -rank_shift
+        } else {
+            rank_shift
+        };
+        backward_shift
     }
 }
